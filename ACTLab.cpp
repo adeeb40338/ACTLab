8487ca7e8ede11424b0ae2e60008e3482079b2a0
adeeb40338 <Adeeb@adeeb-ahmed-hossains-macbook-pro.local>
2012-03-17 12:54:37 +0000

    Added the constructor method back in - to set class parameters. Comments.

--- ACTLab.cpp
+++ ACTLab.cpp
@@ -1,8 +1,22 @@
+// Include the Arduino Library - to access the standard
+// types and constants of the Arduino language.
+
 #include <Arduino.h>
+
+// Include the ACTLab header file where the class methods
+// and properties are declared.
+
 #include <ACTLab.h>
 
-void ACTLabClass::test() {
-  Serial.println("Sketch Started.");
+// Constructor method.
+
+ACTLabClass::ACTLabClass () {}
+
+// Test method. Outputs a serial message. Serial need to
+// already be setup.
+
+void ACTLabClass::serialTest () {
+	Serial.println("Serial Test!");
 }
 
 ACTLabClass ACTLab;
--- ACTLab.h
+++ ACTLab.h
@@ -1,12 +1,39 @@
+
+// If statement to make sure the ACTLab Library is not
+// included more than once.
+
 #ifndef ACTLab_h
 #define ACTLab_h
 
+// Include the Arduino Library - to access the standard
+// types and constants of the Arduino language.
+
 #include <Arduino.h>
 
-class ACTLabClass
-{
-  public:
-    void test();
+// Declare (and spec out) the ACTLabClass class.
+
+class ACTLabClass {
+	
+	// Public methods and properties.
+	
+	public:
+	
+		// Constructor method.
+		
+		ACTLabClass();
+		
+		// Public methods.
+		
+		void serialTest();
+		
+		// Public properties.
+		
+		int qwerty;
+	
+	// Private methods and properties.
+	
+	
+	
 };
 
 extern ACTLabClass ACTLab;
